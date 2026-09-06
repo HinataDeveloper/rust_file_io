69a19bb9a90e02167c590714a5fe23f4f7208050
HinataDeveloper <StubbornStruggle@gmail.com>
2026-09-07 00:15:50 +0330

    Using File IO: Read a directory contents

---
+++ src/snapshots/main_009_20260907_001425.rs
@@ -0,0 +1,56 @@
+// Date: Thu Sep 06 2026
+
+// Project: Learning Chapter 13
+// Goal: Using File IO: Read a directory content
+// Dependency: Without dependency
+
+// rustc 1.100.0-nightly (0ed41eb41 2026-09-04)
+// binary: rustc
+// commit-hash: 0ed41eb4142dda2df61eb1145a312c1a9d62eb56
+// commit-date: 2026-09-04
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.1
+
+// cargo 1.100.0-nightly (b2e9d5f9d 2026-09-02)
+// release: 1.100.0-nightly
+// commit-hash: b2e9d5f9db3fb1c454ab84f10c16508984a266e2
+// commit-date: 2026-09-02
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Fedora 44.0.0 [64-bit]
+
+// Kernel Version: 7.1.13-200.fc44.x86_64
+// Firmware Version: 71CN51WW(V1.21)
+
+use std::fs;
+use std::io;
+
+fn main() {
+    println!("\n");
+
+    const PATH_NAME: &str = "/home/hinata/test";
+
+    let read_dir_result: Result<fs::ReadDir, io::Error> = fs::read_dir(PATH_NAME);
+
+    if let Err(err) = read_dir_result {
+        panic!("An error Occur: {}", err);
+    }
+
+    let dir_read: fs::ReadDir = read_dir_result.unwrap();
+
+    for item in dir_read {
+        let de: fs::DirEntry = item.unwrap();
+        if de.path().is_dir() {
+            println!(" -->> {}", de.file_name().into_string().unwrap());
+        }
+
+        if de.path().is_file() {
+            println!(" -> {}", de.file_name().into_string().unwrap());
+        }
+    }
+
+    println!("\nThe End ...\n");
+}
